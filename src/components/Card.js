c24b9ba718f39af4efd9827e39d4e5ef9ab7cb9c
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-11-28 16:32:42 +0530

    card.js

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import sryled from 'styled-components'
+
+const Container =styled.div`
+  width: 360px;
+  margin-bottom: 45px;
+  cursor: pointer;
+
+`;
+
+const Image =styled.image`
+  width: 100%;
+  height: 202px;
+  background-color: #999;
+
+  
+`;
+
+
+
+const Card = () => {
+    return (
+        <Container>
+            <Image src=""/>
+        </Container>
+    )
+}
+
+
+export default Card
