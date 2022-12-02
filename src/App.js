57815d37c31509a54d2471a6adf5df6c437e49d0
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-12-02 21:14:43 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -2,59 +2,43 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { findByLabelText } from "@testing-library/react";
-import { darkTheme,lightTheme } from "./utils/Theme";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom'
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Video from "./Pages/Video.jsx";
 import { useState } from "react";
 import Home from "./Pages/Home";
 
-const Container =styled.div`
-display: flex;
+const Container = styled.div`
+  display: flex;
 `;
-
-
-
 
 const Main = styled.div`
-flex:7;
-bacground-color: #181818;
-
+  flex: 7;
+  bacground-color: #181818;
 `;
 
-const Wrapper= styled.div`
-padding: 22px 96px;
+const Wrapper = styled.div`
+  padding: 22px 96px;
 `;
-
 
 function App() {
-  const [darkMode,setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
-    // <ThemeProvider theme={(darkMode ? darkTheme : lightTheme)}>
-     <Container>
+    <Container>
       <Router>
-      <Menu setDarkMode={setDarkMode} />
-      <Main>
-        <Navbar />
-           <Wrapper>
-              {/* <Routers> */}
-                <Routes>
-                  <Route index element={<Home/>}/>
-                  <Route exact path="video" element={<Video/>}/>
-                  {/* <Route path=":id" /> */}
-                </Routes>
-              {/* </Routers> */}
-           </Wrapper>
-        
-      </Main>
+        <Menu setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>
+            <Routes>
+              <Route exact path ='/' element={<Home />} />
+              <Route exact path="/video" element={<Video />} />
+            </Routes>
+          </Wrapper>
+        </Main>
       </Router>
- </Container>
-//  </ThemeProvider>
+    </Container>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
+import { Title } from '@mui/icons-material';
 import React from 'react';
-import sryled from 'styled-components'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 
 const Container =styled.div`
-  width: 360px;
+  width: 400px;
   margin-bottom: 45px;
   cursor: pointer;
 
@@ -16,15 +18,55 @@ const Image =styled.image`
   
 `;
 
+const Texts = styled.div`
+  color: white;
+`
+
+const Info = styled.p`
+  font-size: 21px;
+`
+
+const Details =styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 12px;
+
+  
+`;
+const ChannelImage =styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+  `;
+
+  
+const YoutubeContainer = styled.div`
+  width: 390px;
+  height: 310px;
+  overflow: hidden;
+  border-radius: 5%;
+`
 
 
 const Card = () => {
     return (
+        <Link to="/video/test">
         <Container>
-            <Image src=""/>
+            {/* <Image src="https://i9.ytimg.com/vi_webp/k3Vfj-e1Ma4/mqdefault.webp?v=6277c159&sqp=CIjm8JUG&rs=AOn4CLDeKmf_vlMC1q9RBEZu-XQApzm6sA"/> */}
+            <YoutubeContainer><iframe width="400" height="300" src="https://www.youtube.com/embed/038jmlSH3iM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></YoutubeContainer>
+         <Details>
+            <ChannelImage/>
+            <Texts>
+            <Title>Test video</Title>
+            <channelName>Lama Dev</channelName>
+           <Info>660,908 views .1 day ago</Info>
+           </Texts>
+         </Details>
         </Container>
+        </Link>
     )
 }
 
 
-export default Card
+export default Card;
